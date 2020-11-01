f0e867de9a5d710b66052f08435e4da36e7d8b9d
yasaswi1989 <56218300+yasaswi1989@users.noreply.github.com>
2020-11-01 16:22:29 +0530

    Update nodejs-helloworld.js

--- nodejs-helloworld.js
+++ nodejs-helloworld.js
@@ -4,7 +4,8 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(8080, 'localhost');
-"scripts": {
-    "test": "nyc --reporter=text mocha"
- }
+var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'scripts/external_javascript.js';
+    document.body.appendChild(script);
 console.log('Server running at http://localhost:8080/');
